7b2495ae40e2ac929e0c393f74af56de401013c8
PublicUniversalFriend <KMEck@gmx.net>
2021-04-13 09:40:21 +0200

    new scripts

---
+++ Assets/Scripts/Scene1QuestionScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene1QuestionScript : MonoBehaviour
+{
+    public GameObject falseImage;
+
+    public void False()
+    {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        falseImage.SetActive(true);
+        yield return new WaitForSeconds(3);
+        Application.LoadLevel(1);
+    }
+}
+
